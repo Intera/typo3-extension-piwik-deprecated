ffd8a0d692258f76e2d2c4c1c58aba8de6ca9393
kaystrobach <kaystrobach@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-17 18:27:12 +0000

    TER 1.0.4
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/piwik/trunk@43791 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "piwik".
 #
-# Auto generated 12-02-2011 10:54
+# Auto generated 17-02-2011 19:25
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Adds Piwik JS-Code (http://piwik.org/) to your pages',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.0.3',
+	'version' => '1.0.4',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:7:{s:18:"class.tx_piwik.php";s:4:"aee8";s:12:"ext_icon.gif";s:4:"d803";s:17:"ext_localconf.php";s:4:"a9b8";s:14:"doc/manual.sxw";s:4:"7706";s:21:"static/notracker.html";s:4:"b38e";s:28:"static/notracker_beuser.html";s:4:"3482";s:19:"static/tracker.html";s:4:"4932";}',
+	'_md5_values_when_last_written' => 'a:10:{s:18:"class.tx_piwik.php";s:4:"aee8";s:12:"ext_icon.gif";s:4:"d803";s:17:"ext_localconf.php";s:4:"a9b8";s:14:"ext_tables.php";s:4:"e80b";s:14:"doc/manual.sxw";s:4:"7706";s:21:"static/notracker.html";s:4:"b38e";s:28:"static/notracker_beuser.html";s:4:"3482";s:19:"static/tracker.html";s:4:"4932";s:26:"static/piwik/constants.txt";s:4:"8e03";s:22:"static/piwik/setup.txt";s:4:"f522";}',
 	'suggests' => array(
 	),
 );
