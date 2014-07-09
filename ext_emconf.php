4c37f26a876e3df2bae1e7c963ff5963ce125994
Alexander Stehlik <alexander.stehlik@gmail.com>
2014-07-09 17:57:00 +0200

    [TASK] Raised required TYPO3 version

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.3.0-4.7.99',
+			'typo3' => '4.3.0-6.3.99',
 		),
 		'conflicts' => array(
 		),
